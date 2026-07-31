ef8b5e2351ad97d04af7c1d467128addbf34f6b1
TotiManuel <manuel.mandaio@gmail.com>
2026-07-31 01:18:12 -0300

    RM all

--- src/service/dashboardInstitucion.service.ts
+++
@@ -1,5 +0,0 @@
-import { api } from "../api/api";
-
-export const obtenerDashboardInstitucion = async () => {
-  return await api("/dashboard/institucion");
-};
--- src/service/dashboardProveedor.service.ts
+++
@@ -1,5 +0,0 @@
-import { api } from "../api/api";
-
-export const obtenerDashboardProveedor = async () => {
-  return await api("/dashboard/proveedor");
-};
